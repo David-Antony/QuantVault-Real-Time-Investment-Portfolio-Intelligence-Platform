146da5005c08891fabb9e9fd222c217fd74bb9bb
DAVID ANTONY A <davidantony@karunya.edu.in>
2026-07-24 23:05:56 +0530

    db: add prisma seed script template

---
+++ prisma/seed.js
@@ -0,0 +1,17 @@
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt');
+const prisma = new PrismaClient();
+
+async function main() {
+  console.log('Seeding database...');
+  // Logic to seed database can be added here
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
